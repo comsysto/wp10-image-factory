fc6570e3db29d91b9a21846f439bbbd0f143fbc6
Kresimir Sutalo <k.sutalo@reply.de>
2024-09-17 14:38:51 +0200

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = "${random_pet.prefix.id}-rg"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 1
+}
