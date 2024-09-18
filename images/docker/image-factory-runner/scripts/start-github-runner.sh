8e1d92e3431dd582b8d0928299e283938488f8fa
Silvija Tovernic <s.tovernic@reply.de>
2024-09-18 19:19:53 +0200

    add: podman settings and packer

--- images/docker/image-factory-runner/scripts/start-github-runner.sh
+++ images/docker/image-factory-runner/scripts/start-github-runner.sh
@@ -8,7 +8,8 @@ GITHUB_RUNNER_NAME=$(hostname)
 echo "Getting runner registration token from GitHub..."
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-# Add this part once PAT token generation is set up on the runner host VM - new PAT token will be generated on each start
+# Add this part once PAT token generation is set up on the runner host VM 
+#   - new PAT token will be generated as a pre task to each runner container start
 #
 # echo "Removing PAT token from runner filesystem"
 # rm -rf /.pat/.token
