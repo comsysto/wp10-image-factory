672c34c60a718c3448ac42877e0ae9f38c001038
Kresimir Sutalo <k.sutalo@reply.de>
2024-09-19 17:56:07 +0200

    imp: output

--- terraform/modules/runner/outputs.tf
+++ terraform/modules/runner/outputs.tf
@@ -2,6 +2,10 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.jumphost.public_ip_address
 }
 
+output "private_ip_address" {
+  value = azurerm_linux_virtual_machine.runner.private_ip_address
+}
+
 output "key_data" {
   value = azapi_resource_action.ssh_public_key_gen.output.publicKey
 }
