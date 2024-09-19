6289304bd01d72cfe6b4999d6c180f4e0375323d
Silvija Tovernic <s.tovernic@reply.de>
2024-09-19 20:42:28 +0200

    fix: trivy install

--- images/packer/image-factory-vm/scripts/setup-vm.sh
+++ images/packer/image-factory-vm/scripts/setup-vm.sh
@@ -38,6 +38,6 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 # Set up Trivy repository and install Trivy
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
-echo "deb https://aquasecurity.github.io/trivy-repo/deb \$(lsb_release -sc) main" | tee -a /etc/apt/sources.list.d/trivy.list
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update
 apt-get install -y trivy
