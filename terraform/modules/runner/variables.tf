364ad3d3599e7c2a60f03dda80a9efc34da75b9c
Kresimir Sutalo <k.sutalo@reply.de>
2024-09-20 16:31:27 +0200

    feat: use image gallery runner image

--- terraform/modules/runner/variables.tf
+++ terraform/modules/runner/variables.tf
@@ -36,6 +36,12 @@ variable "username" {
 
 variable "ssh_key_name" {
   type        = string
-  description = "Name of the generated SSH key for the VM"
+  description = "Name of the generated SSH key for the VM."
   default     = "runnersshkey"
+}
+
+
+variable "runner_image_id" {
+  type        = string
+  description = "Shared Image Gallery ID of the runner VM image."
 }
