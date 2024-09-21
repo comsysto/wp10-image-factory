df83dd459c8c7f98a6a2666dbd9c17c6db0204e7
Silvija Tovernic <s.tovernic@reply.de>
2024-09-21 14:41:29 +0200

    test new dockerfile

--- images/docker/image-factory-runner/scripts/start-github-runner.sh
+++ images/docker/image-factory-runner/scripts/start-github-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GITHUB_OWNER=$GITHUB_OWNER
-GITHUB_REPOSITORY=$GITHUB_REPOSITORY
+# GITHUB_OWNER=$GITHUB_OWNER
+# GITHUB_REPOSITORY=$GITHUB_REPOSITORY
 GITHUB_TOKEN=$(cat /.pat/.token)
 GITHUB_RUNNER_NAME="if-runner-$(hostname)"
 GITHUB_LABELS="wp10-image-factory"
@@ -10,6 +10,15 @@ echo "Log in to container registry:"
 podman login "$CONTAINER_REGISTRY" \
   --username "$CONTAINER_REGISTRY_USERNAME" \
   --password "$CONTAINER_REGISTRY_PASSWORD"
+
+echo "Configure registries:"
+cat <<EOF > $HOME/.config/containers/registries.conf
+[registries]
+registries = ["$CONTAINER_REGISTRY"]
+
+[registries.search]
+registries = ["$CONTAINER_REGISTRY"]
+EOF
 
 # REG_TOKEN Token will be available only during container startup
 echo "Getting runner registration token from GitHub..."
