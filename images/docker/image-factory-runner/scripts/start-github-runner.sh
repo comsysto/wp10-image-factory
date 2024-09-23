e90ca3d3297c2eea007de9ea979228fcfd1d716d
stovernic <34249785+stovernic@users.noreply.github.com>
2024-09-23 09:28:41 +0200

    feat: configure image factory vm with ansible (#9)
    Workflow runs:
    - Ansible (env controlled using env var VM_ENV):
    - On VM in air-gapped environment:
    https://github.com/comsysto/wp10-image-factory/actions/runs/10973783705/job/30473550404
    (**problem accessing ACR from the VM**)
    - On VM in public subnet:
    https://github.com/comsysto/wp10-image-factory/actions/runs/10973783705/job/30471539771
    
    - Docker runner build:
    -
    https://github.com/comsysto/wp10-image-factory/actions/runs/10974200763
    
    ---------
    
    Co-authored-by: Silvija Tovernic <s.tovernic@reply.de>

--- images/docker/image-factory-runner/scripts/start-github-runner.sh
+++ images/docker/image-factory-runner/scripts/start-github-runner.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-GITHUB_OWNER=$GITHUB_OWNER
-GITHUB_REPOSITORY=$GITHUB_REPOSITORY
+# GITHUB_OWNER=$GITHUB_OWNER
+# GITHUB_REPOSITORY=$GITHUB_REPOSITORY
 GITHUB_TOKEN=$(cat /.pat/.token)
-GITHUB_RUNNER_NAME="image-factory-runner"
+GITHUB_RUNNER_NAME="if-runner-$(hostname)"
+GITHUB_LABELS="wp10-image-factory"
 
+echo "Log in to container registry:"
+podman login "$CONTAINER_REGISTRY" \
+  --username "$CONTAINER_REGISTRY_USERNAME" \
+  --password "$CONTAINER_REGISTRY_PASSWORD"
+
+echo "Configure registries:"
+cat <<EOF > $HOME/.config/containers/registries.conf
+[registries]
+registries = ["$CONTAINER_REGISTRY", "docker.io"]
+
+[registries.search]
+registries = ["$CONTAINER_REGISTRY", "docker.io"]
+EOF
+
+# REG_TOKEN Token will be available only during container startup
 echo "Getting runner registration token from GitHub..."
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-# Add this part once PAT token generation is set up on the runner host VM 
-#   - new PAT token will be generated as a pre task to each runner container start
-#
-# echo "Removing PAT token from runner filesystem"
-# rm -rf /.pat/.token
+echo "Deleting PAT token from runner filesystem..."
+sudo umount /.pat
+sudo rmdir /.pat
+
+# QEMU setup
+sudo update-binfmts --enable qemu-aarch64
+sudo update-binfmts --enable qemu-arm
 
 echo "Connect runner to GitHub:"
 cd actions-runner
 ./config.sh \
   --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
   --token ${REG_TOKEN} \
-  --name ${RUNNER_NAME} \
+  --name ${GITHUB_RUNNER_NAME} \
+  --labels ${GITHUB_LABELS} \
   --unattended \
   --ephemeral \
   --replace \
